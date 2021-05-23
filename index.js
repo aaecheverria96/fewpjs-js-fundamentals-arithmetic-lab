1c69f7cc39e9e9fba0ecf5adcaa0b29aed6cd1cb
Your Username <you@example.com>
2021-05-23 18:43:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Write your code here 
+var num1 = 31; 
+var num2 = 2; 
+var multiply = num1 * num2; 
+
+var random = Math.floor(Math.random() * 10);  
+
+var num3 = 3; 
+var num4 = 12; 
+var mod = num4 / num3 ; 
+
+const mySet = new Set() 
+mySet.add(1) 
+mySet.add(20)  
+Math.max(mySet);
